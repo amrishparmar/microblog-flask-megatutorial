bb947047a82d0457e88b7ccf206350759106baac
Amrish Parmar <amrish.parmar@me.com>
2018-01-24 15:10:40 +0000

    finished part2

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,22 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'amrish'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!',
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!',
+        },
+        {
+            'author': {'username': 'Simon'},
+            'body': 'I like cheese',
+        },
+        {
+            'author': {'username': 'Jess'},
+            'body': 'I want to go to the park',
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
