8f2e6b4efce11a84e71849cf58331e402fa6a4ad
Amrish Parmar <amrish.parmar@me.com>
2018-01-26 16:38:05 +0000

    completed part4

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<User id={self.id} username={self.username} email={self.email} ' \
+               f'password_hash={self.password_hash}>'
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Post id={self.id} body={self.body} timestamp={self.timestamp} ' \
+               f'user_id={self.user_id}>'
--- config.py
+++ config.py
@@ -2,5 +2,11 @@ import os
 import secrets
 
 
-class Config():
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
